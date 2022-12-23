8f85fdfd4571cdce9a08ed841c9048ba8f65f1c6
username <gjaviles7@gmail.com>
2022-12-23 15:35:20 -0500

    configured Next-Auth and MoralisNextAuth

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { createClient, configureChains, defaultChains, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { SessionProvider } from 'next-auth/react';
+
+const { provider, webSocketProvider } = configureChains(defaultChains, [publicProvider()]);
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </WagmiConfig>
+  );
 }
