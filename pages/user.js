6d0f9b3407d0bb523cd472f47a01b420e52e86d1
username <gjaviles7@gmail.com>
2022-12-26 16:20:36 -0500

    axios is able to get NFTs

--- pages/user.js
+++ pages/user.js
@@ -4,19 +4,27 @@ import axios from "axios";
 // gets a prop from getServerSideProps
 function User({ user }) {
 
-    const [nfts, setNFTs] = useState()
-
-    const getNFTs = () => {
-        axios
-        .get(`https://online-store.herokuapp.com/api/online-store/items/${id}`)
-        .then((response) => {
-            props.setNFTs(response.data);
+    const options = {
+        method: 'GET',
+        url: 'https://deep-index.moralis.io/api/v2/0x5D3Af3b111B07Af18A14cDA6B5027eec53E581F3/nft',
+        params: {chain: 'eth', format: 'decimal', normalizeMetadata: 'false'},
+        headers: {accept: 'application/json', 'X-API-Key': '90aMzzA9q0jMTFt2SUqJ2t1CWVRaIVUBErIJcDwYINiHz2vtquYggZMOzf9FKQZL'}
+      };
+      
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.error(error);
         });
-    };
+
+        console.log(user.address)
 
     return (
         <div>
-            <h4>User session:</h4>
+            <h4>My NFTs</h4>
             <pre>{JSON.stringify(user, null, 2)}</pre>
             <button onClick={() => signOut({ redirect: '/signin' })}>Sign out</button>
         </div>
