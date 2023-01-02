d723b9a6109e0921a9358f02f19bceac293c4b20
username <gjaviles7@gmail.com>
2023-01-02 16:19:58 -0500

    able to add address from frontend

--- pages/nfts/trackedWallets.js
+++ pages/nfts/trackedWallets.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/router'
 import axios from "axios";
 import Link from 'next/link'
 
 
 function TrackedWallets({ user }) {
 
+    const router = useRouter();
     const [trackedAddresses, setTrackedAddresses] = useState([])
     const [loaded, setLoaded] = useState(false)
+    const [addressToAdd, setAddressToAdd] = useState({
+        addresses: ""
+    })
+    const [addressToDelete, setAddressToDelete] = useState({
+        addresses: ""
+    })
 
 
     const getTrackedAddresses = () => {
@@ -26,31 +34,55 @@ function TrackedWallets({ user }) {
 
       console.log(trackedAddresses)
 
+    const addAddressCall = async (e) => {
+        e.preventDefault()
+        try {
+            // eslint-disable-next-line no-unused-vars
+            const change = await axios.put(`http://localhost:8080/api/lungo-backend/addAddress/${user.address.toLowerCase()}`, addressToAdd)
+                router.push('/user')
+        }
+        catch (err) {
+            console.log(err)
+        }
+    } 
+
+    const handleAddAddress = (e) => {
+        e.preventDefault()
+        const addAddressInput = {...addressToAdd};
+        addAddressInput[e.target.name] = e.target.value;
+        setAddressToAdd(addAddressInput);
+    }
+
     return (
         <>
             <div className="tracked-addresses">
-            <div>
-                My Tracked Wallets ({trackedAddresses.length})
-            </div>
-            {trackedAddresses.length > 0 && (
-                <>
-                    <div className="addresses-container">
-                        {trackedAddresses?.map((trackedAddress, key) => {
-                            return (
-                                <Link 
-                                    href='/trackedAddresses/[trackedAddress]'
-                                    as={`trackedAddresses/${trackedAddress}`} 
-                                    key={key}
-                                >
-                                    <div style={{ width: "70px" }}>
-                                        {trackedAddress}
-                                    </div>
-                                </Link>
-                            )
-                        })}
-                    </div>
-                </>
-            )}
+                <div>
+                    My Tracked Wallets ({trackedAddresses.length})
+                </div>
+                <h1>Add Wallet</h1>
+                <form className='address-box' onSubmit={addAddressCall}>
+                    <input className='input' placeholder="Address" name="addresses" value={addressToAdd.addresses} onChange={handleAddAddress} required></input>
+                    <button className='item-button'>Add</button>
+                </form>
+                {trackedAddresses.length > 0 && (
+                    <>
+                        <div className="addresses-container">
+                            {trackedAddresses?.map((trackedAddress, key) => {
+                                return (
+                                    <Link 
+                                        href='/trackedAddresses/[trackedAddress]'
+                                        as={`trackedAddresses/${trackedAddress}`} 
+                                        key={key}
+                                    >
+                                        <div style={{ width: "70px" }}>
+                                            {trackedAddress}
+                                        </div>
+                                    </Link>
+                                )
+                            })}
+                        </div>
+                    </>
+                )}
             </div>
         </>
     )
