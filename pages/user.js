75936ca91896583cc5e087a18bebcf8bd0e5404c
username <gjaviles7@gmail.com>
2023-01-05 14:34:28 -0500

    moved NFT Container out of pages

--- pages/user.js
+++ pages/user.js
@@ -1,6 +1,6 @@
 import { getSession, signOut } from 'next-auth/react';
 import { useEffect, useState } from 'react';
-import NFTContainer from './nfts';
+import NFTContainer from '.';
 import TrackedWallets from './trackedWallets';
 import axios from "axios";
 
