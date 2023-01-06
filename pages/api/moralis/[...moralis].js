1a7c75d70263cb4b20b7c43ef215da88bf2180ff
username <gjaviles7@gmail.com>
2023-01-05 19:01:36 -0500

    updated domain again on ...moralis

--- pages/api/moralis/[...moralis].js
+++ pages/api/moralis/[...moralis].js
@@ -3,7 +3,7 @@ import { MoralisNextApi } from '@moralisweb3/next';
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY,
   authentication: {
-    domain: 'https://lungo.vercel.app',
+    domain: 'lungo.vercel.app',
     uri: process.env.NEXTAUTH_URL,
     timeout: 120,
   },
